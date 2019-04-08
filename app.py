210183aa8aa7c1d9676c40fd4f13d7a002f9774d
Aarno Aukia <aarno.aukia@vshn.ch>
2019-04-08 23:36:27 +0300

    remove reconnecting

--- app.py
+++ app.py
@@ -9,17 +9,10 @@ of OLD_HOST strings are replaced with NEW_HOST
 from autobahn.twisted.websocket import WebSocketServerProtocol, WebSocketServerFactory
 from autobahn.twisted.websocket import WebSocketClientProtocol, WebSocketClientFactory
 from autobahn.twisted.websocket import connectWS
-from twisted.internet.protocol import ReconnectingClientFactory
-import urllib
 import time
 import txaio
-from twisted.internet import reactor, ssl, task
-import os
 from dotenv import load_dotenv
-
-BACKEND = os.environ.get('BACKEND')
-OLD_HOST = os.environ.get('OLD_HOST')
-NEW_HOST = os.environ.get('NEW_HOST')
+import os
 
 
 class WebsocketInfoServerProtocol(WebSocketServerProtocol):
@@ -30,42 +23,28 @@ class WebsocketInfoServerProtocol(WebSocketServerProtocol):
 
     def onOpen(self):
         print("Client WebSocket connection open.")
-        if self.proxyfactory is None:
-            # self.proxyfactory = MyClientFactory(url=BACKEND)
-            self.proxyfactory = WebSocketClientFactory(url=BACKEND)
-            self.proxyfactory.setProtocolOptions(autoPingInterval=5, autoPingTimeout=2)
-            self.proxyfactory.protocol = WebsocketInfoProxyProtocol
-            self.proxyfactory.clientconnection = self
-            connectWS(self.proxyfactory)
+        print(
+            "starting proxy backend connection attempt: {0}".format(
+                os.environ.get("BACKEND")
+            )
+        )
+        self.proxyfactory = WebSocketClientFactory(url=os.environ.get("BACKEND"))
+        self.proxyfactory.setProtocolOptions(autoPingInterval=5, autoPingTimeout=2)
+        self.proxyfactory.protocol = WebsocketInfoProxyProtocol
+        self.proxyfactory.clientconnection = self
+        connectWS(self.proxyfactory)
 
     def onMessage(self, payload, isBinary):
         if isBinary:
-            print("Binary message received: {0} bytes".format(len(payload)))
+            print("Client Binary message received: {0} bytes".format(len(payload)))
         else:
-            print("Text message received: {0}".format(payload.decode("utf8")))
-        # reactor.callFromThread(self.proxyfactory.proxyproto.sendMessage, self.proxyfactory.proxyproto, payload)
-        # self.proxyfactory.proxyproto.sendMessage(self.proxyfactory.proxyproto, payload)
+            print("Client Text message received: {0}".format(payload.decode("utf8")))
         self.proxyfactory.proxyproto.sendMessage(payload)
 
     def onClose(self, wasClean, code, reason):
-        print("WebSocket connection closed: {0}".format(reason))
+        print("Client WebSocket connection closed: {0}".format(reason))
         self.proxyfactory.proxyproto.sendClose()
 
-
-#class MyClientFactory(ReconnectingClientFactory, WebSocketClientFactory):
-#    proxyproto = None
-#
-#    def startedConnecting(self, connector):
-#        log.msg("Proxy Started to connect: {0} {1}".format(self, self.url))
-#
-#    def clientConnectionLost(self, connector, reason):
-#        log.msg("Proxy Lost connection. Reason: {}".format(reason))
-#        ReconnectingClientFactory.clientConnectionLost(self, connector, reason)
-#
-#    def clientConnectionFailed(self, connector, reason):
-#        log.msg("Proxy Connection failed. Reason: {}".format(reason))
-#        ReconnectingClientFactory.clientConnectionFailed(self, connector, reason)
-#
 
 class WebsocketInfoProxyProtocol(WebSocketClientProtocol):
     def onConnect(self, request):
@@ -81,7 +60,9 @@ class WebsocketInfoProxyProtocol(WebSocketClientProtocol):
         else:
             print("Proxy Text message received: {0}".format(payload.decode("utf8")))
         message = payload.decode("utf8")
-        message = message.replace(OLD_HOST, NEW_HOST)
+        message = message.replace(
+            os.environ.get("OLD_HOST"), os.environ.get("NEW_HOST")
+        )
         self.factory.clientconnection.sendMessage(message.encode("utf8"))
 
     def onClose(self, wasClean, code, reason):
