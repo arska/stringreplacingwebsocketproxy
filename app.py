314b24ad3c89a273b8d5d1324f4e1633987c4928
Aarno Aukia <aarno.aukia@vshn.ch>
2019-04-13 23:25:30 +0300

    refactor help into README, add comments

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
 """
 Websocket proxy with string replacement
-
-Websocket server providing an websocket endpoint at /
-all websocket messages are forwarded to Websocket endpoint BACKEND
-The connection to BACKEND can be authenticated using SSL_CLIENT_CERT/SSL_CLIENT_KEY
-Verification for the CA-certificate in SSL_CLIENT_CA is implemented but untested
-on the answer of BACKEND string replacement can be performed: all occurrences
-of OLD_HOST strings are replaced with NEW_HOST
 """
 from autobahn.twisted.websocket import WebSocketServerProtocol, WebSocketServerFactory
 from autobahn.twisted.websocket import WebSocketClientProtocol, WebSocketClientFactory
@@ -21,12 +14,20 @@ from OpenSSL import crypto
 
 
 class WebsocketInfoServerProtocol(WebSocketServerProtocol):
+    """
+    This is the "server" protocol answering to incoming client connections
+    """
+
     proxyfactory = None
 
     def onConnect(self, request):
         print("Client connecting: {0}".format(request.peer))
 
     def onOpen(self):
+        """
+        When the websocket connection upgrade has happened create the BACKEND connection
+        """
+
         print("Client WebSocket connection open.")
         print(
             "starting proxy backend connection attempt: {0}".format(
@@ -56,6 +57,10 @@ class WebsocketInfoServerProtocol(WebSocketServerProtocol):
         connectWS(self.proxyfactory, contextFactory=sslfactory)
 
     def onMessage(self, payload, isBinary):
+        """
+        when the client sends data forward to BACKEND
+        """
+
         if isBinary:
             print("Client Binary message received: {0} bytes".format(len(payload)))
         else:
@@ -63,13 +68,25 @@ class WebsocketInfoServerProtocol(WebSocketServerProtocol):
         self.proxyfactory.proxyproto.sendMessage(payload)
 
     def onClose(self, wasClean, code, reason):
+        """
+        When the client closes the connection also close the BACKEND
+        """
         print("Client WebSocket connection closed: {0}".format(reason))
+        # the backend connection might never have been opened
         if self.proxyfactory is not None:
             self.proxyfactory.proxyproto.sendClose()
 
 
 class WebsocketInfoProxyProtocol(WebSocketClientProtocol):
+    """
+    This is the "client" protocol talking to the BACKEND server
+    """
+
     def onConnect(self, request):
+        """
+        Update the originating factory with a reference to this protocol for the "server protocol" corresponding to "our" client connection to be able to send data to this BACKEND connection
+        If we never successfully connect this remains None and the server protocol will of course throw errors
+        """
         print("Proxy connecting: {0}".format(request.peer))
         self.factory.proxyproto = self
 
@@ -77,6 +94,10 @@ class WebsocketInfoProxyProtocol(WebSocketClientProtocol):
         print("Proxy WebSocket connection open.")
 
     def onMessage(self, payload, isBinary):
+        """
+        If we get data from BACKEND forward it to our client connection, performing string replacement if requested
+        """
+
         if isBinary:
             print("Proxy Binary message received: {0} bytes".format(len(payload)))
         else:
@@ -88,6 +109,9 @@ class WebsocketInfoProxyProtocol(WebSocketClientProtocol):
         self.factory.clientconnection.sendMessage(message.encode("utf8"))
 
     def onClose(self, wasClean, code, reason):
+        """
+        If the BACKEND connection gets closed also close the client connection
+        """
         print("Proxy WebSocket connection closed: {0}".format(reason))
         self.factory.clientconnection.sendClose()
 
@@ -100,12 +124,12 @@ if __name__ == "__main__":
     from twisted.internet import reactor
 
     load_dotenv()
-    # log.startLogging(sys.stdout)
-    txaio.start_logging(level="debug")
+    log.startLogging(sys.stdout)
+    # txaio.start_logging(level="debug")
 
     factory = WebSocketServerFactory()
     factory.protocol = WebsocketInfoServerProtocol
     factory.setProtocolOptions(autoPingInterval=10, autoPingTimeout=3)
 
-    reactor.listenTCP(8080, factory)
+    reactor.listenTCP(os.environ.get("PORT", 8080), factory)
     reactor.run()
